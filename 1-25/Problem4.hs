8e12bdb48929373351b1f17dbc997f310826bd29
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2012-08-23 21:28:01 +0200

    [TASK] Solve problem 4 with Haskell.

---
+++ 1-25/Problem4.hs
@@ -0,0 +1,22 @@
+-- A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+-- Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+isPalindrome s = if (show s) == (reverse (show s)) 
+    then True
+    else False
+
+nPalindrome n c l = if even (length (show (n * c))) && isPalindrome (n * c)
+    then if c == 100
+        then nPalindrome (n-1) 999 (l ++ [(n * c)])
+        else if n == 100 
+            then l
+            else nPalindrome n (c-1) (l ++ [(n * c)])
+    else if c == 100
+        then nPalindrome (n-1) 999 l
+        else if n == 100 
+            then l
+            else nPalindrome n (c-1) l
+
+-- Run the function as such: maxPalindrom
+maxPalindrom = maximum (nPalindrome 999 999 [])
